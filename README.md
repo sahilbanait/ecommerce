d9f09dba3d5aa70e43a893c0d1a46489c8f3e887
Sahil Kumar <sahil.saini526@gmail.com>
2022-10-15 22:04:36 +0800

    changes to navbar and product page

--- README.md
+++ README.md
@@ -1,5 +1,85 @@
-# E-commerce product page
+# Sneaker E-commerce Page
 
 ![Design preview for the E-commerce product page coding challenge](./design/desktop-preview.jpg)
 
 
+## Technologies used
+
+* #### [HTML5]()
+    * The project used HTML5 to create the basic elements and content of my website
+* #### [CSS3]()
+    * The project uses CSS3 to add custom styles to the elements and content of my website
+* #### [Javascript]()
+    * The project used Vanilla Javascript in order to add functionality to the app
+* #### [Font Awesome]()
+    * The project used Font Awesome for the nav bar element and footer elements
+* #### [Webstorm,Intelli J]()
+    * I have used Webstorm and Intelli J IDE to write the project code
+* #### [Git]()
+    * I have used Git as a version control system to regularly add and commit changes made to project in Webstorm,
+      before pushing them to GitHub
+* #### [Github]()
+    * GitHub to store the committed changes made into the project and also to deploy the app in live environment
+* #### [Bootstrap](https://getbootstrap.com/)
+    *  For this project i have used the **Bootstrap** library for styling of the element
+***
+
+## Features
+
+All the pages are incorporated with Navigation
+
+* Navigation 
+    * Collection - Main product page 
+    * Men
+    * Women
+    * About
+    * Contact
+    * Cart - Allow users to see  all the items ready to buy.
+    * User profile
+  
+* Product
+  * Images - User can see various images of product
+  * Add to cart - Allow user to add items into the cart 
+
+## Overview
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Open a lightbox gallery by clicking on the large product image
+- Switch the large product image by clicking on the small thumbnail images
+- Add items to the cart
+- View the cart and remove items from it
+***
+
+## Deployment
+
+The hosting platform that I've used for my project is GitHub Pages.
+To deploy my website to GitHub pages, I used the following steps:
+
+1. Initialised Git using the git init command.
+
+2. Added all files to the Staging area (Git) using the git add . command.
+
+3. Committed the files to Git using the git commit -m "Initial commit" command.
+
+4. From IDE shared the project called **ecommerce** which automatically create a Repository with same name in
+   GitHub.
+
+5. First Commit the code changes into GitHub using IDE git tool
+
+6. Regularly committed the code changes using same tool
+
+## Project Link
+
+* [Repository](https://github.com/sahilbanait/ecommerce)
+* [Live](http://sahilbanait.me/ecommerce/)
+
+## Running Code Locally
+
+To run my code locally, users can download a local copy of my code to their desktop by completing the following steps:
+
+1. Go to my GitHub repository with above link.
+2. Click on 'Clone or download'.
+3. Click on 'Download ZIP'.
+4. Once downloaded, extract the zip file's contents and run my website locally.
--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Images
-    let expandImg = document.getElementById("mainImage");
+
+    let expandMainImg = document.getElementById("mainImage");
     const next = document.getElementById("next");
     const previous = document.getElementById("previous");
     const thumbnailImages = document.querySelectorAll(".thumbnail-img");
-    // Add to Cart controls
+
     const cartAdd = document.getElementById('cart-increase');
     const cartRemove = document.getElementById('cart-decrease');
     let cartNum = document.getElementById('cart-number');
     const addToCart = document.getElementById('add-to-cart-btn');
     let cartBadge = document.getElementById('cart-badge');
     const numOfProductInCart = document.querySelector('.num-in-cart-badge');
-    const productPrice = 125.00;
+    const PRODUCT_PRICE = 125.00;
     const totalPrice = document.querySelector('.total-price');
-    // Shopping Cart content
+   
     const shoppingCart = document.getElementById('shopping-cart');
     const cartContentContainer = document.querySelector('.cart-content-container');
     const emptyCart = document.querySelector('.empty-cart');
@@ -21,54 +21,54 @@ document.addEventListener("DOMContentLoaded", () => {
     const deleteIcon = document.querySelector('.delete-icon');
 
     
-    if (!expandImg.src) {
-        expandImg.src = "static/images/image-product-1.jpg";
+    if (!expandMainImg.src) {
+        expandMainImg.src = "static/images/image-product-1.jpg";
     }
 
     // Loop through thumbnail images
     thumbnailImages.forEach((img) => {
         img.addEventListener("click", () => {
-            // set the main product image.src to the current image.src
-            expandImg.src = img.src;
-            // Remove the active class from previous image and active class to the current image
+            
+            expandMainImg.src = img.src;
+            
             let current = document.getElementsByClassName('active');
             current[0].className = current[0].className.replace(" active", "");
             img.classList.add('active');
         });
     });
 
-    // Set Event listener for the next button to view next product image
+    // next product image
     next.addEventListener("click", () => {
-        currentImg = expandImg.src;
+        currentImg = expandMainImg.src;
         let i = 0;
         while (i < thumbnailImages.length - 1) {
             if (currentImg === thumbnailImages[i].src) {
-                expandImg.src = thumbnailImages[i + 1].src;
+                expandMainImg.src = thumbnailImages[i + 1].src;
             }
             i++;
         }
     });
 
-    // Set Event listener for the previous button to view previous product image
+    //  previous product image
     previous.addEventListener("click", () => {
-        currentImg = expandImg.src;
+        currentImg = expandMainImg.src;
         let i = thumbnailImages.length - 1;
         while (i > 0) {
             if (currentImg === thumbnailImages[i].src) {
-                expandImg.src = thumbnailImages[i - 1].src;
+                expandMainImg.src = thumbnailImages[i - 1].src;
             }
             i--;
         }
     });
 
-    // Increase number of items to add to cart
+    //  add to cart
     cartAdd.addEventListener('click', () => {
         if(parseInt(cartNum.textContent) < 20) {
             cartNum.textContent =  parseInt(cartNum.textContent) + 1
         }
     })
 
-    // decrease number of items to add to cart
+    // Remove from cart
     cartRemove.addEventListener('click', () => {
         if (parseInt(cartNum.textContent) > 0) {
             cartNum.textContent =  parseInt(cartNum.textContent) - 1
@@ -86,7 +86,7 @@ document.addEventListener("DOMContentLoaded", () => {
             cartBadge.style.display = "block"
             cartBadge.textContent = parseInt(cartBadge.textContent) + parseInt(cartNum.textContent);
             numOfProductInCart.textContent = cartBadge.textContent;
-            totalPrice.textContent = ` $${(parseFloat(productPrice) * parseFloat(numOfProductInCart.textContent)).toFixed(2)}`;
+            totalPrice.textContent = ` $${(parseFloat(PRODUCT_PRICE) * parseFloat(numOfProductInCart.textContent)).toFixed(2)}`;
         }
         cartNum.textContent = 0;
     })
